b1a6ddb7f78202c252813dbe5e421a29111bc0b5
Inna Gosteva <innagosteva31@yandex.ru>
2023-02-04 15:00:16 +0500

    mongoconnect

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
@@ -23,7 +24,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true, autoIndex: true, useUnifiedTopology: true,
 });
 
